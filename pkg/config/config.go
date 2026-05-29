ca89a3a60c2f4f254df35764b3f9ebc53d035c14
HT4w5 <ht4w5x@gmail.com>
2026-05-29 20:41:33 +0800

    docs(config): document Validatable

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,8 @@
 package config
 
+// Validatable is a config object that can be validated.
 type Validatable interface {
+	// Validate returns error if config is invalid.
+	// Configs must be validated before passed to object constructors.
 	Validate() error
 }
