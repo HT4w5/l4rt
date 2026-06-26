bbd6e53b8d5ecd82ef2f193f2ff191f5f8516410
HT4w5 <ht4w5x@gmail.com>
2026-06-27 01:05:12 +0800

    feat(context): add benchmarks for counters

---
+++ pkg/utils/context/counter_bench_test.go
@@ -0,0 +1,37 @@
+package context
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+)
+
+func BenchmarkSimpleIDCounter(b *testing.B) {
+	for i := range runtime.NumCPU() {
+		i += 1
+		b.Run(fmt.Sprintf("GOMAXPROCS=%d", i), func(b *testing.B) {
+			runtime.GOMAXPROCS(i)
+			c := SimpleIDCounter{}
+			b.RunParallel(func(p *testing.PB) {
+				for p.Next() {
+					c.GetID()
+				}
+			})
+		})
+	}
+}
+
+func BenchmarkRandomShardedIDCounter(b *testing.B) {
+	for i := range runtime.NumCPU() {
+		i += 1
+		b.Run(fmt.Sprintf("GOMAXPROCS=%d", i), func(b *testing.B) {
+			runtime.GOMAXPROCS(i)
+			c := NewRandomShardedIDCounter(i)
+			b.RunParallel(func(p *testing.PB) {
+				for p.Next() {
+					c.GetID()
+				}
+			})
+		})
+	}
+}
